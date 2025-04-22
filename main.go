1041e4b07e8b77d46d9666257a961d642bbea4e1
Arie <ariejlo19@gmail.com>
2025-04-22 07:28:38 +0700

    checking error

--- main.go
+++ main.go
@@ -7,5 +7,10 @@ import (
 
 func main() {
 	fmt.Println("test")
-	os.Create("b.txt")
+	f, err := os.Create("b.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("success", f.Name())
 }
