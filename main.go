1767e8436c1d6ce37230186897192f593fd65ec8
Arie <ariejlo19@gmail.com>
2025-04-22 15:06:37 +0700

    test

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 
 func main() {
 	fmt.Println("test")
-	f, err := os.Create("b.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("success", f.Name())
+	os.Create("b.txt")
+
 }
